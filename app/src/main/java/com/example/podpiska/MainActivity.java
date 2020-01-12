691ec3f3b5cf0dcd736252e09bfa1b81822d0059
serg0 <serg.01@bk.ru>
2020-01-12 18:38:09 +0300

    fixup! 2 btn clear and ok

--- app/src/main/java/com/example/podpiska/MainActivity.java
+++ app/src/main/java/com/example/podpiska/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     Button btn2 ;
     EditText name;
     EditText email;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,31 +24,27 @@ public class MainActivity extends AppCompatActivity {
     }
     private void init(){
         btn1 = findViewById(R.id.btn1);
+        btn2 =  findViewById(R.id.btn2);
         name = findViewById(R.id.editEmail);
         email = findViewById(R.id.underTxt);
+        textView = findViewById(R.id.txt);
     }
     private void initOk () {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String str = getString(R.string.txt_second)  + " " + name.getText().toString()+" "+getString(R.string.txt_first) + " " + email.getText().toString();
-                TextView textView = findViewById(R.id.txt);
                 textView.setText(str);
-
-
             }
         });
     }
     private void initClear(){
-        btn2 =  findViewById(R.id.btn2);
+
         btn2.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView underTxt = findViewById(R.id.underTxt);
-                underTxt.setText("");
-                TextView editEmail = findViewById(R.id.editEmail);
-                editEmail.setText("");
+                name.setText("");
+                email.setText("");
             }
         });
     }
